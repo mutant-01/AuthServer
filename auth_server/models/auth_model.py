2ec528fd1bca8b6dd2e7a2709540fe3d45703562
s.sadabadi <s.sadabadi@alibaba.ir>
2018-08-11 19:37:43 +0430

    add many to many as rest subresource base view

--- auth_server/models/auth_model.py
+++ auth_server/models/auth_model.py
@@ -1,4 +1,4 @@
-from peewee import CharField, ForeignKeyField, DateTimeField, TextField
+from peewee import CharField, ForeignKeyField, TextField, CompositeKey
 from auth_server import db_wrapper
 
 
@@ -21,6 +21,7 @@ class Roles(db_wrapper.Model):
 class UserRoles(db_wrapper.Model):
     class Meta:
         table_name = 'user_roles'
+        primary_key = CompositeKey('user_id', 'role_id')
 
     user_id = ForeignKeyField(Users, null=False, on_delete='CASCADE')
     role_id = ForeignKeyField(Roles, null=False, on_delete='CASCADE')
@@ -36,6 +37,8 @@ class Resources(db_wrapper.Model):
 class ResourceRoles(db_wrapper.Model):
     class Meta:
         table_name = 'resource_roles'
+        primary_key = CompositeKey('resource_id', 'role_id')
+
     resource_id = ForeignKeyField(Resources, null=False, on_delete='CASCADE')
     role_id = ForeignKeyField(Roles, null=False, on_delete='CASCADE')
 
@@ -75,3 +78,43 @@ def get_resources_by_roles(roles: set, resources: set) -> list:
     if result[0] is None:
         return []
     return result[0]
+
+
+def get_all_many_many_as_sub(base_table, sub_table, relation_table, key_to_base, key_to_sub, base_id, fields):
+    """"""
+    results = db_wrapper.database.execute_sql(
+        "select {sub}.* from {base} "
+        "inner join {relation} on ({base}.id={relation}.{key_base}) "
+        "inner join {sub} on ({sub}.id={relation}.{key_sub}) WHERE {relation}.{key_base}={base_id}".format(
+            base=base_table,
+            sub=sub_table,
+            relation=relation_table,
+            key_base=key_to_base,
+            key_sub=key_to_sub,
+            base_id=base_id
+        )
+    ).fetchall()
+    if results is None:
+        return []
+    return [{k: v for k, v in zip(fields, result)} for result in results]
+
+
+def get_one_many_many_as_sub(base_table, sub_table, relation_table, key_to_base, key_to_sub, base_id, sub_id, fields):
+    """"""
+    result = db_wrapper.database.execute_sql(
+        "select {sub}.* from {base} "
+        "inner join {relation} on ({base}.id={relation}.{key_base}) "
+        "inner join {sub} on ({sub}.id={relation}.{key_sub}) "
+        "WHERE {relation}.{key_base}={base_id} AND {relation}.{key_sub}={sub_id}".format(
+            base=base_table,
+            sub=sub_table,
+            relation=relation_table,
+            key_base=key_to_base,
+            key_sub=key_to_sub,
+            base_id=base_id,
+            sub_id=sub_id
+        )
+    ).fetchone()
+    if result is None:
+        return {}
+    return {k: v for k, v in zip(fields, result)}
--- auth_server/views/base_views.py
+++ auth_server/views/base_views.py
@@ -4,6 +4,8 @@ from flask.views import MethodView
 from marshmallow import Schema, ValidationError
 from peewee import DoesNotExist, IntegrityError
 from auth_server import db_wrapper
+from auth_server.models.auth_model import get_all_many_many_as_sub, get_one_many_many_as_sub
+from auth_server.serializers.identity_serializers import IdSerializer
 
 
 class BasicCrudView(MethodView):
@@ -69,6 +71,75 @@ class BasicCrudView(MethodView):
 
     def delete(self, id):
         if self.model.delete().where(self.model.id == id).execute() > 0:
+            return "", 204
+        else:
+            return "Resource Does not exist", 404
+
+
+class ManyManySubResource(MethodView):
+    """represent a many to many relation as sub resource.
+
+    the relation must be assumed to have a direction from one of source tables"""
+    #  todo tight dependency to db, replace peewee with some other ORM
+    base_table = ''
+    relation_table = ''
+    relation_model = db_wrapper.Model
+    relation_key_to_base = ''
+    relation_key_to_sub = ''
+    sub_table = ''
+    fields = []
+
+    # sub resource serializer
+    serializer = Schema
+
+    def get(self, base_id, id):
+        """Returns all resources or one by id"""
+        serializer_obj = self.serializer()
+        # returns all records of the model
+        if id is None:
+            db_data = get_all_many_many_as_sub(
+                self.base_table, self.sub_table, self.relation_table,
+                self.relation_key_to_base, self.relation_key_to_sub, base_id, self.fields
+            )
+            return serializer_obj.dumps(db_data, many=True), 200
+        # returns a record by id
+        else:
+            db_data = get_one_many_many_as_sub(
+                self.base_table, self.sub_table, self.relation_table,
+                self.relation_key_to_base, self.relation_key_to_sub, base_id, id, self.fields
+            )
+            if not db_data:
+                return "Resource Does not exist", 404
+            return serializer_obj.dumps(db_data), 200
+
+    def post(self, base_id):
+        """creates new resource"""
+        serializer_obj = IdSerializer()
+        json_data = request.get_json()
+        if json_data is None:
+            return "Invalid json", 400
+        try:
+            data = serializer_obj.load(json_data)
+        except ValidationError as e:
+            getLogger().exception(e)
+            return "validation error for: {}".format(e.field_names), 422
+        else:
+            data[self.relation_key_to_base] = base_id
+            data[self.relation_key_to_sub] = data.pop("id")
+            try:
+                self.relation_model.insert(**data).execute()
+            except IntegrityError as e:
+                getLogger().exception(e)
+                return "Resource already exists", 409
+            return "", 204
+
+    def delete(self, base_id, id):
+        count = self.relation_model.delete().where(
+            getattr(self.relation_model, self.relation_key_to_base) == base_id
+            ,
+            getattr(self.relation_model, self.relation_key_to_sub) == id
+        ).execute()
+        if count > 0:
             return "", 204
         else:
             return "Resource Does not exist", 404
