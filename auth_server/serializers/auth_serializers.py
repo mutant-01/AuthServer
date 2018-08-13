9ee48bbfe7dffff77d8394a9ed2833182debe818
s.sadabadi <s.sadabadi@alibaba.ir>
2018-08-13 15:05:50 +0430

    fix empty roles as None in token claims

--- auth_server/serializers/auth_serializers.py
+++ auth_server/serializers/auth_serializers.py
@@ -1,4 +1,4 @@
-from marshmallow import validate, fields, Schema
+from marshmallow import validate, fields, Schema, pre_dump
 
 from auth_server.serializers.base_serializers import StrictSchema
 
@@ -16,8 +16,17 @@ class Claims(Schema):
     class Meta:
         dateformat = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-    roles = fields.List(fields.Int())
-    iss_dt = fields.DateTime()
+    roles = fields.List(fields.Int(), required=True)
+    iss_dt = fields.DateTime(required=True)
+
+    @pre_dump
+    def convert_none_to_empty(self, data):
+        try:
+            if len(data["roles"]) == 1 and data["roles"][0] is None:
+                data["roles"] = []
+        except KeyError:
+            data["roles"] = []
+        return data
 
 
 class AuthorizeInput(StrictSchema):
