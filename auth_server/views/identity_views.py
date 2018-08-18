c09d7dbefd557217a9cdcb3e60b943d7d48c3f04
s.sadabadi <s.sadabadi@alibaba.ir>
2018-08-18 10:50:59 +0430

    add user_info update

--- auth_server/views/identity_views.py
+++ auth_server/views/identity_views.py
@@ -1,10 +1,13 @@
-from flask import Blueprint
+from logging import getLogger
+from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from marshmallow import ValidationError
 from peewee import DoesNotExist
 from auth_server.config import url_prefix
 from auth_server.models.auth_model import Users, Roles, Resources, UserRoles, ResourceRoles
 from auth_server.serializers.identity_serializers import UserSerializer, RoleSerializer, ResourceSerializer
 from auth_server.utils.jwt import access_required
+from auth_server.utils.view_utils import json_or_400
 from auth_server.views.base_views import BasicCrudView, ManyManySubResource
 
 identity_bp = Blueprint('identity', __name__)
@@ -95,3 +98,22 @@ def get_user_info():
         return UserSerializer().dumps(Users.get_info_by_id(id)), 200
     except DoesNotExist:
         return "resource does not exists anymore", 404
+
+
+@identity_bp.route('/user_info', methods=['PATCH'])
+@jwt_required
+@json_or_400
+def update_user_info():
+    id = get_jwt_identity()
+    try:
+        in_data = UserSerializer(partial=True).load(request.get_json())
+    except ValidationError as e:
+        getLogger().exception(e)
+        getLogger().error("validation error for update_user_info request: {}".format(e))
+        return "bad format input for fields: {}".format(e.field_names), 422
+    else:
+        count = Users.update(**in_data).where(Users.id == id).execute()
+        if count < 1:
+            return "resource does not exists anymore", 404
+        else:
+            return "", 204
